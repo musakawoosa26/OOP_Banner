15dc997d8660237c0a897728bf6359fb45937dbf
musakawoosa26 <musakawoosa1@gmail.com>
2026-02-17 22:51:38 +0530

    Made UC3

--- UC3.java
+++ UC3.java
@@ -1,15 +1,16 @@
-// Hint: Use String.join(" ", ...) to join the parts of each line with spaces.
-
-System.out.println(String.join("", *** "" *****"));
-
-System.out.println(String.join("", ** " "));
-
-System.out.println(String.join("", "** **", "** "));
-
-System.out.println(String.join("", "** **", " *****"));
-
-System.out.println(String.join(" ", "** **"" **"));
-
-System.out.println(String.join(" ", " ** **"));
-
-System.out.println(String.join("", " *** "," *****"));
+/*
+@version 1.0
+@author Musa
+ */
+ 
+ public class Helloworld{
+  public static oid main(string args[]){
+	 System.out.println("   ***   " + "  " + "   ***  " + "  " + " ****** " + "  " + "   *****");
+     System.out.println(" **   **" + "  " + "  **   **" + "  " + " **   **" + "  " + " **");
+     System.out.println("**     **" + "  " + "**     **" + "  " + "**   **" + "  "+ "**");
+     System.out.println("**     **" + "  " + "**     **" + "  " + "****** " + "  " + "  *****");
+     System.out.println("**     **" + "  " +  "**     **" + "  " + "**    " + "  " + "      **");
+     System.out.println(" **   **" + "  " + "  **   **" + "  " + " **    " + "  " + " **   **");
+     System.out.println("   ***   " + "  " + "   ***   " + "  " + "**    " + "  " + "  *****   ");
+  }
+ }
