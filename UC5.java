cc4a88f0583ad9b07e80c80830baa826a72bd141
musakawoosa26 <musakawoosa1@gmail.com>
2026-03-02 01:02:19 +0530

    UC5 added

--- UC5.java
+++ UC5.java
@@ -1,30 +1,17 @@
+import java.util.*;
 public class UC5 {
     public static void main(String[] args) {
-
-        String[] line1 = {
-                " *****   *****   *****   ***** "
-        };
-
-        String[] line2 = {
-                "*     * *     * *     * *      "
-        };
-
-        String[] line3 = {
-                "*     * *     * ******* *******"
-        };
-
-        String[] line4 = {
-                "*     * *     * *             *"
-        };
-
-        String[] line5 = {
-                " *****   *****  *        ***** "
-        };
-
-        System.out.println(String.join("", line1));
-        System.out.println(String.join("", line2));
-        System.out.println(String.join("", line3));
-        System.out.println(String.join("", line4));
-        System.out.println(String.join("", line5));
+        Scanner sc= new Scanner(System.in);
+        String lines[] = {
+        (String.join(" ","   ***   ", "    ***   ", "  ******  ", "    *****")),
+        (String.join(" "," **   ** ", "  **   ** ", " **    ** ", "  **")),
+        (String.join(" ","**     **", " **     **", " **     **", " **")),
+        (String.join(" ","**     **", " **     **", " **    ** ", "  *****")),
+        (String.join(" ","**     **", " **     **", " ******   ", "      **")),
+        (String.join(" "," **   ** ", "  **   ** ", " **       ", "      **")),
+        (String.join(" ","   ***   ", "    ***   ", " **       ", "  *****"))};
+        for(int i=0;i<lines.length;i++){
+            System.out.println(lines[i]);
+        }
     }
 }
