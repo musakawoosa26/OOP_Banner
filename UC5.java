49b1218b62891df711744fdb0d1e830a7db6d1a0
musakawoosa26 <musakawoosa1@gmail.com>
2026-03-02 00:40:42 +0530

    UC5 added

---
+++ UC5.java
@@ -0,0 +1,30 @@
+public class UC5 {
+    public static void main(String[] args) {
+
+        String[] line1 = {
+                " *****   *****   *****   ***** "
+        };
+
+        String[] line2 = {
+                "*     * *     * *     * *      "
+        };
+
+        String[] line3 = {
+                "*     * *     * ******* *******"
+        };
+
+        String[] line4 = {
+                "*     * *     * *             *"
+        };
+
+        String[] line5 = {
+                " *****   *****  *        ***** "
+        };
+
+        System.out.println(String.join("", line1));
+        System.out.println(String.join("", line2));
+        System.out.println(String.join("", line3));
+        System.out.println(String.join("", line4));
+        System.out.println(String.join("", line5));
+    }
+}
